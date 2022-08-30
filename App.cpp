3e4576a705f4ea3196172ed685d7f73ad49fe913
Patryk Kiepas <kiepas.patryk@gmail.com>
2022-08-30 21:44:18 +0200

    The simplest looping service

---
+++ App.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Quepas on 30.08.2022.
+//
+
+#include "App.hpp"
+
+void App::initialize(Poco::Util::Application &self) {
+    Application::initialize(self);
+}
+
+void App::uninitialize() {
+    Application::uninitialize();
+}
+
+int App::main(const std::vector<std::string> &args) {
+    while(true) {}
+}
---
+++ App.hpp
@@ -0,0 +1,29 @@
+//
+// Created by Quepas on 30.08.2022.
+//
+
+#ifndef POCO_WINDOWS_SERVICE_APP_H
+#define POCO_WINDOWS_SERVICE_APP_H
+
+
+#include <Poco/Util/ServerApplication.h>
+
+class App : public Poco::Util::ServerApplication {
+
+public:
+    App() = default;
+
+    ~App() = default;
+
+protected:
+    void initialize(Application &self);
+
+    void uninitialize();
+
+    int main(const std::vector<std::string> &args);
+
+
+};
+
+
+#endif //POCO_WINDOWS_SERVICE_APP_H
